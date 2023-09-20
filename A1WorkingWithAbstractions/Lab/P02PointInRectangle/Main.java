64ef85386589b2c2a35d73ad2b449a24225a33bc
figtereiscoding <quererqabv@gmail.com>
2023-09-20 15:51:09 +0300

    Create Main.java

---
+++ A1WorkingWithAbstractions/Lab/P02PointInRectangle/Main.java
@@ -0,0 +1,30 @@
+package A1WorkingWithAbstraction.P02PointInRectangle;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] coordinates = getCoordinates(scanner);
+        Point bottomLeft = new Point(coordinates[0], coordinates[1]);
+        Point topRight = new Point(coordinates[2], coordinates[3]);
+        Rectangle rectangle = new Rectangle(bottomLeft, topRight);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            int[] pointCoordinates = getCoordinates(scanner);
+            Point point = new Point(pointCoordinates[0], pointCoordinates[1]);
+
+            System.out.println(rectangle.contains(point));
+        }
+    }
+
+    private static int[] getCoordinates(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+}
