253104be75385a5b84489294cb450406d32f0c2f
figtereiscoding <quererqabv@gmail.com>
2023-09-20 15:51:22 +0300

    Create Rectangle.java

---
+++ A1WorkingWithAbstractions/Lab/P02PointInRectangle/Rectangle.java
@@ -0,0 +1,23 @@
+package A1WorkingWithAbstraction.P02PointInRectangle;
+
+public class Rectangle {
+    private Point bottomLeft;
+    private Point topRight;
+
+    public Rectangle(Point bottomLeft, Point topRight) {
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+    }
+
+    public boolean contains(Point point) {
+       boolean isInHorizontal = this.bottomLeft.getX() <= point.getX() &&
+               this.topRight.getX() >= point.getX();
+
+       boolean isInVertical = this.bottomLeft.getY() <= point.getY()
+                && this.topRight.getY() >= point.getY();
+
+       boolean isInRectangle = isInHorizontal && isInVertical;
+
+       return isInRectangle;
+    }
+}
