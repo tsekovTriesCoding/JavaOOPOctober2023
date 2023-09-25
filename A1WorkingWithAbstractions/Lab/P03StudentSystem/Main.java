f8a2df2493316a61e800b9f8791bb38a3c106955
figtereiscoding <quererqabv@gmail.com>
2023-09-25 16:55:18 +0300

    Create Main.java

---
+++ A1WorkingWithAbstractions/Lab/P03StudentSystem/Main.java
@@ -0,0 +1,20 @@
+package A1WorkingWithAbstraction.Lab.P03StudentSystem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CommandExecutor executor = new CommandExecutor();
+        while (true) {
+            String[] input = scanner.nextLine().split(" ");
+
+            if (CommandExecutor.isExitCommand(input[0])) {
+                break;
+            }
+            executor.executeCommand(input[0], input);
+
+        }
+    }
+}
