8a2818c95aec2bd05154e3d34874b2b81695e34b
figtereiscoding <quererqabv@gmail.com>
2023-09-25 16:55:10 +0300

    Create CommandExecutor.java

---
+++ A1WorkingWithAbstractions/Lab/P03StudentSystem/CommandExecutor.java
@@ -0,0 +1,37 @@
+package A1WorkingWithAbstraction.Lab.P03StudentSystem;
+
+public class CommandExecutor {
+    private StudentSystem studentSystem;
+    private static final String EXIT_COMMAND = "Exit";
+    private static final String SHOW_STUDENT_COMMAND = "Show";
+    private static final String CREATE_STUDENT_COMMAND = "Create";
+
+    public CommandExecutor() {
+        this.studentSystem = new StudentSystem();
+    }
+
+    public static boolean isExitCommand(String command) {
+        return command.equals(EXIT_COMMAND);
+    }
+
+    public void executeCommand(String command, String[] data) {
+
+        switch (command) {
+            case CREATE_STUDENT_COMMAND:
+                String name = data[1];
+                int age = Integer.parseInt(data[2]);
+                double grade = Double.parseDouble(data[3]);
+                studentSystem.createStudent(name, age, grade);
+                break;
+            case SHOW_STUDENT_COMMAND:
+                String studentName = data[1];
+                String result = studentSystem.showStudent(studentName);
+
+                if (result != null) {
+                    System.out.println(result);
+                }
+                break;
+        }
+
+    }
+}
