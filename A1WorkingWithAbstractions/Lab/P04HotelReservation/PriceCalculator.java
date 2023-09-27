373ef61039b1afdc07bd31144b6c53fdb70d5250
figtereiscoding <quererqabv@gmail.com>
2023-09-27 17:31:23 +0300

    Create PriceCalculator.java

---
+++ A1WorkingWithAbstractions/Lab/P04HotelReservation/PriceCalculator.java
@@ -0,0 +1,10 @@
+package A1WorkingWithAbstraction.Lab.P04HotelReservation;
+
+public class PriceCalculator {
+
+    public static double calculatePrice(double pricePerDay, int numberOfDays, Season season, Discount discount) {
+        double basePrice = pricePerDay * numberOfDays * season.getMultiplier();
+
+        return basePrice * (1 - discount.getDiscountPercent());
+    }
+}
