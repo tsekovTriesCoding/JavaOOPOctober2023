d09bd105cd8408701d676befa17b4b6091a96b54
figtereiscoding <quererqabv@gmail.com>
2023-09-30 17:07:19 +0300

    Create Main.java

---
+++ A1WorkingWithAbstractions/Exercise/P01CardSuit/Main.java
@@ -0,0 +1,11 @@
+package A1WorkingWithAbstraction.Exercise.P01CardSuit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Suits:");
+
+        for (CardSuit cardSuit : CardSuit.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", cardSuit.ordinal(), cardSuit);
+        }
+    }
+}
