d446de9ab336c0f2f1447c46b278f8b7560d1e04
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-05 20:10:22 +0300

    Create Main.java

---
+++ A2Encapsulation/Exercise/P04PizzaCalories/Main.java
@@ -0,0 +1,40 @@
+package A2Encapsulation.Exercise.P04PizzaCalories;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] pizzaInfo = scanner.nextLine().split("\\s+");
+        String name = pizzaInfo[1];
+        int numberOfTopping = Integer.parseInt(pizzaInfo[2]);
+        String[] doughInfo = scanner.nextLine().split("\\s+");
+        String flourType = doughInfo[1];
+        String bakingTechnique = doughInfo[2];
+        double weight = Double.parseDouble(doughInfo[3]);
+        try {
+            Pizza pizza = new Pizza(name, numberOfTopping);
+            Dough dough = new Dough(flourType, bakingTechnique, weight);
+            pizza.setDough(dough);
+
+            String input = scanner.nextLine();
+            while (!input.equals("END")) {
+                String[] toppingInfo = input.split("\\s+");
+                String toppingType = toppingInfo[1];
+                double toppingWeight = Double.parseDouble(toppingInfo[2]);
+                Topping topping = new Topping(toppingType, toppingWeight);
+                pizza.addTopping(topping);
+
+                input = scanner.nextLine();
+            }
+
+            System.out.printf("%s - %.2f", pizza.getName(), pizza.getOverallCalories());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
