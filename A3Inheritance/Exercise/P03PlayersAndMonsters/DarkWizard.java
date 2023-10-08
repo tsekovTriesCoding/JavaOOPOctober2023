7d6d4d5b6598706e18e399b1727db5bd230d2865
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-08 22:42:37 +0300

    Create DarkWizard.java

---
+++ A3Inheritance/Exercise/P03PlayersAndMonsters/DarkWizard.java
@@ -0,0 +1,7 @@
+package A3Inheritance.Exercise.P03PlayersAndMonsters;
+
+public class DarkWizard extends Wizard {
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
