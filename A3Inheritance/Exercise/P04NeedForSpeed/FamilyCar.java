d80ef8ceabaece2ec58809fda5e90331efb57d57
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-09 19:33:13 +0300

    Create FamilyCar.java

---
+++ A3Inheritance/Exercise/P04NeedForSpeed/FamilyCar.java
@@ -0,0 +1,7 @@
+package A3Inheritance.Exercise.P04NeedForSpeed;
+
+public class FamilyCar extends Car {
+    public FamilyCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
