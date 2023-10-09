7f19255efe57fe2f079e4046db8fe2813245e20a
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-09 19:34:03 +0300

    Create ColdBeverage.java

---
+++ A3Inheritance/Exercise/P05Restaurant/Beverage/ColdBeverage.java
@@ -0,0 +1,9 @@
+package A3Inheritance.Exercise.P05Restaurant.Beverage;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage {
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
