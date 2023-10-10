eb292c4675113735bf278ea7293452dc5bd120fa
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-10 20:42:45 +0300

    Create MainDish.java

---
+++ A3Inheritance/Exercise/P05Restaurant/Food/MainDish.java
@@ -0,0 +1,9 @@
+package A3Inheritance.Exercise.P05Restaurant.Food;
+
+import java.math.BigDecimal;
+
+public class MainDish extends Food {
+    public MainDish(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
