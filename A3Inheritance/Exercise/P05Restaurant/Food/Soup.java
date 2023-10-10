1f9dc24f86595d624b371658f77d0c986b4b5a93
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-10 20:42:55 +0300

    Create Soup.java

---
+++ A3Inheritance/Exercise/P05Restaurant/Food/Soup.java
@@ -0,0 +1,9 @@
+package A3Inheritance.Exercise.P05Restaurant.Food;
+
+import java.math.BigDecimal;
+
+public class Soup extends Starter {
+    public Soup(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
