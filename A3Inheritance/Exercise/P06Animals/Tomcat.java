35ed6a45a5e0620f6d3086b91cbae214aee36e21
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-11 20:56:24 +0300

    Create Tomcat.java

---
+++ A3Inheritance/Exercise/P06Animals/Tomcat.java
@@ -0,0 +1,14 @@
+package A3Inheritance.Exercise.P06Animals;
+
+public class Tomcat extends Cat {
+    public static final String GENDER = "Male";
+
+    public Tomcat(String name, int age) {
+        super(name, age, GENDER);
+    }
+
+    @Override
+    public String produceSound() {
+        return "MEOW";
+    }
+}
