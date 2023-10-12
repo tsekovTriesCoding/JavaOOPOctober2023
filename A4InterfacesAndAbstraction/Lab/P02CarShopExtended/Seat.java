af73e14645693b83679c2c42c0236ebd94eb2a4d
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-12 22:46:31 +0300

    Create Seat.java

---
+++ A4InterfacesAndAbstraction/Lab/P02CarShopExtended/Seat.java
@@ -0,0 +1,24 @@
+package A4InterfacesAndAbstraction.Lab.P02CarShopExtended;
+
+public class Seat extends CarImpl implements Sellable {
+    private Double price;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced, Double price) {
+        super(model, color, horsePower, countryProduced);
+        this.price = price;
+    }
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced) {
+        super(model, color, horsePower, countryProduced);
+    }
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%n%s sells for %f", super.toString(), getModel(), getPrice());
+    }
+}
