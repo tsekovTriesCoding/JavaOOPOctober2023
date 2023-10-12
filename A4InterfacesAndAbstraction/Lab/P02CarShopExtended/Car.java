6f161e9a535f301d9a62f0f3331f7af499b78504
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-12 22:46:10 +0300

    Create Car.java

---
+++ A4InterfacesAndAbstraction/Lab/P02CarShopExtended/Car.java
@@ -0,0 +1,16 @@
+package A4InterfacesAndAbstraction.Lab.P02CarShopExtended;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    Integer TIRES = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+
+}
