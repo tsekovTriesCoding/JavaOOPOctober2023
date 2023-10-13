652d6ddeee7209960ff49865d9cda3e4859589e1
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-13 19:00:58 +0300

    Create Chinese.java

---
+++ A4InterfacesAndAbstraction/Lab/P04SayHelloExtended/Chinese.java
@@ -0,0 +1,13 @@
+package A4InterfacesAndAbstraction.Lab.P04SayHelloExtended;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
