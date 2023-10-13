5d6eb415f56ddfaa510acaf29bb84a179bd20b09
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-13 19:00:51 +0300

    Create Bulgarian.java

---
+++ A4InterfacesAndAbstraction/Lab/P04SayHelloExtended/Bulgarian.java
@@ -0,0 +1,14 @@
+package A4InterfacesAndAbstraction.Lab.P04SayHelloExtended;
+
+
+public class Bulgarian extends BasePerson {
+
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
