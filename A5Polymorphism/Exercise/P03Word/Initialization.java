e610ff617f1bebb3260b5f718507ca013a67412e
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-29 20:22:39 +0200

    added solution

---
+++ A5Polymorphism/Exercise/P03Word/Initialization.java
@@ -0,0 +1,10 @@
+package A5Polymorphism.Exercise.P03Word;
+
+public class Initialization {
+
+    public static CommandInterface buildCommandInterface(StringBuilder text) {
+        CommandInterface commandInterface = new CommandImpl(text);
+        commandInterface.init();
+        return commandInterface;
+    }
+}
